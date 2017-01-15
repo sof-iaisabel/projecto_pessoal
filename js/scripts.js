05683fff66a23117ecc22c18b75c0a3f8d6a6e34
sof-iaisabel <sofiaisabel-desousa@hotmail.com>
2017-01-15 20:02:06 +0000

    alterações finais feitas ao projecto.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+/* Função para abrir o botao */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Fecha o Menu se clickar fora dele
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
